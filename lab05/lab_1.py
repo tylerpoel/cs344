45e06c4443c4930ece4b2cb01afed3910fab2f78
tylerpoel <58230944+tylerpoel@users.noreply.github.com>
2020-03-06 18:01:11 -0500

    lab05 final

--- lab05/lab_1.py
+++ lab05/lab_1.py
@@ -9,7 +9,7 @@ Updated by: Tyler Poel, for lab05 in CS 344, at Calvin University
 Date: March 6, 2020
 '''
 
-from probability import BayesNet, enumeration_ask, elimination_ask, gibbs_ask
+from probability import BayesNet, enumeration_ask, elimination_ask, gibbs_ask, likelihood_weighting, rejection_sampling
 
 # Utility variables
 T, F = True, False
@@ -31,9 +31,25 @@ print(elimination_ask('Burglary', dict(JohnCalls=T, MaryCalls=T), burglary).show
 print(gibbs_ask('Burglary', dict(JohnCalls=T, MaryCalls=T), burglary).show_approx())
 # See the explanation of the algorithms in AIMA Section 14.4.
 
+# rejection_sampling() and likelihood_weighting() are also approximation algorithms.
+print(rejection_sampling('Burglary', dict(JohnCalls=T, MaryCalls=T), burglary).show_approx())
+print(likelihood_weighting('Burglary', dict(JohnCalls=T, MaryCalls=T), burglary).show_approx())
+
 # P(Alarm | burglary ∧ ¬earthquake)
 print('5.1 i:  \t' + enumeration_ask('Alarm', dict(Burglary=T, Earthquake=F), burglary).show_approx())
 # P(John | burglary ∧ ¬earthquake)
 print('5.1 ii: \t' + enumeration_ask('John', dict(Burglary=T, Earthquake=F), burglary).show_approx())
 # P(Burglary | alarm)
 print('5.1 iii:\t' + enumeration_ask('Burglary', dict(Alarm=T), burglary).show_approx())
+
+'''
+Answers to section 5.4 of lab05, for CS 344:
+The approximation algorithms give answers that one would expect given their name. They're approximately the right 
+answer, but not exactly. These algorithms also don't return the same answer every time, but bounce around in 
+proximity to the true answer. This is because these algorithms work using random sampling. Every time they're run, 
+they will be computing from a different random sample, which changes the answer. This is also why the answer isn't
+exactly the same as the true answer. These algorithms are doing things to decrease the problem space in different
+ways, mostly by not using certain probabilities or variables and through sampling, and not computing from the entire
+problem space. Because of this the approximation algorithms are always going to give an answer slightly different than
+the true answer.
+'''
--- lab05/lab_3.py
+++ lab05/lab_3.py
@@ -22,18 +22,38 @@ print("5.3 ai: \t" + enumeration_ask('Raise', dict(Sunny=T), happiness).show_app
 # P(Raise | happy ^ sunny)
 print("5.3 aii:\t" + enumeration_ask('Raise', dict(Sunny=T, Happy=T), happiness).show_approx())
 '''
+The answers here make sense. I wouldn't expect sunniness, or happiness and sunniness, to have much of an effect on
+getting a raise or not. Raise and Sunny here are both independent variables; the only thing that can slightly affect
+one of them is happiness.
+
 Hand Calculations:
 5.3 ai.
     P(Raise | Sunny) - confounding variable Happy
     = α Σ P( R, S, H)
     = α Σ P(R) * P(S) * P(H | R, S)
     = α < P(R) * P(S) * (P(H) + P(not H)), P(not R) * P(S) * (P(H) + P(not H)) >
+    = α < 0.01 * 0.7 * (1.0 + 0.0), 0.99 * 0.7 * (0.7 + 0.3) >
     = α <0.007, 0.093>
     = <0.01, 0.99>
     
+5.3 aii.
+    P(Raise | happy ∧ sunny)
+    = α Σ P(R, H, S)
+    = α Σ P(R) * P(S) * P(H | S, R)
+    = α < P(R) * P(S) * P(H | S, R), P(not R) * P(S) * P(H | S, not R) >
+    = α < 0.01 * 0.7 * 1.0, 0.99 * 0.7, 0.7 >
+    = α <0.07, 0.4851>
+    = <0.0142, 0.986>
 '''
 
 # P(Raise | happy)
 print("5.3 bi: \t" + enumeration_ask('Raise', dict(Happy=T), happiness).show_approx())
 # P(Raise | happy ^ ¬sunny)
 print("5.3 bii:\t" + enumeration_ask('Raise', dict(Sunny=F, Happy=T), happiness).show_approx())
+
+'''
+These answers also made sense to me. While happiness could possibly have a slight effect on getting a raise, that 
+causal relationship mainly flows the other way, with a raise affecting happiness. With the second one, it makes
+sense that the additional knowledge of it not being sunny helps get a slightly better percent than almost 100%
+false, but still gives a small likelihood overall.
+'''
