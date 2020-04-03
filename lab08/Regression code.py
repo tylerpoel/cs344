70d2e5e784fab95462c27c994a8c9682b35d5221
tylerpoel <58230944+tylerpoel@users.noreply.github.com>
2020-04-03 15:51:03 -0400

    lab08 final

---
+++ lab08/Regression code.py
@@ -0,0 +1,112 @@
+'''
+For lab08 in CS 344 at Calvin University
+Code found at:
+https://github.com/fchollet/deep-learning-with-python-notebooks/blob/master/3.7-predicting-house-prices.ipynb
+Student: Tyler Poel
+Date: April 3, 2020
+
+Exercise 8.4
+'''
+
+import keras
+keras.__version__
+from keras.datasets import boston_housing
+from keras import models
+from keras import layers
+import numpy as np
+
+(train_data, train_targets), (test_data, test_targets) =  boston_housing.load_data()
+
+# Normalizing the data
+mean = train_data.mean(axis=0)
+train_data -= mean
+std = train_data.std(axis=0)
+train_data /= std
+
+test_data -= mean
+test_data /= std
+
+
+# Building our network
+def build_model():
+    # Because we will need to instantiate
+    # the same model multiple times,
+    # we use a function to construct it.
+    model = models.Sequential()
+    model.add(layers.Dense(64, activation='relu',
+                           input_shape=(train_data.shape[1],)))
+    model.add(layers.Dense(64, activation='relu'))
+    model.add(layers.Dense(1))
+    model.compile(optimizer='rmsprop', loss='mse', metrics=['mae'])
+    return model
+
+
+# Validating our approach using K-fold validation
+k = 4
+num_val_samples = len(train_data) // k
+num_epochs = 100
+all_scores = []
+for i in range(k):
+    print('processing fold #', i)
+    # Prepare the validation data: data from partition # k
+    val_data = train_data[i * num_val_samples: (i + 1) * num_val_samples]
+    val_targets = train_targets[i * num_val_samples: (i + 1) * num_val_samples]
+
+    # Prepare the training data: data from all other partitions
+    partial_train_data = np.concatenate(
+        [train_data[:i * num_val_samples],
+         train_data[(i + 1) * num_val_samples:]],
+        axis=0)
+    partial_train_targets = np.concatenate(
+        [train_targets[:i * num_val_samples],
+         train_targets[(i + 1) * num_val_samples:]],
+        axis=0)
+
+    # Build the Keras model (already compiled)
+    model = build_model()
+    # Train the model (in silent mode, verbose=0)
+    model.fit(partial_train_data, partial_train_targets,
+              epochs=num_epochs, batch_size=1, verbose=0)
+    # Evaluate the model on the validation data
+    val_mse, val_mae = model.evaluate(val_data, val_targets, verbose=0)
+    all_scores.append(val_mae)
+
+print(np.mean(all_scores))
+
+# from keras import backend as K
+#
+# # Some memory clean-up
+# K.clear_session()
+#
+# k = 4
+# num_val_samples = len(train_data) // k
+# num_epochs = 500
+# all_mae_histories = []
+# for i in range(k):
+#     print('processing fold #', i)
+#     # Prepare the validation data: data from partition # k
+#     val_data = train_data[i * num_val_samples: (i + 1) * num_val_samples]
+#     val_targets = train_targets[i * num_val_samples: (i + 1) * num_val_samples]
+#
+#     # Prepare the training data: data from all other partitions
+#     partial_train_data = np.concatenate(
+#         [train_data[:i * num_val_samples],
+#          train_data[(i + 1) * num_val_samples:]],
+#         axis=0)
+#     partial_train_targets = np.concatenate(
+#         [train_targets[:i * num_val_samples],
+#          train_targets[(i + 1) * num_val_samples:]],
+#         axis=0)
+#
+#     # Build the Keras model (already compiled)
+#     model = build_model()
+#     # Train the model (in silent mode, verbose=0)
+#     history = model.fit(partial_train_data, partial_train_targets,
+#                         validation_data=(val_data, val_targets),
+#                         epochs=num_epochs, batch_size=1, verbose=0)
+#     mae_history = history.history['val_mean_absolute_error']
+#     all_mae_histories.append(mae_history)
+#
+# average_mae_history = [np.mean([x[i] for x in all_mae_histories]) for i in range(num_epochs)]
+# print(average_mae_history)
+
