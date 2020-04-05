f81e721ae5334e5a08e305a4b76c126965dee2f9
tylerpoel <58230944+tylerpoel@users.noreply.github.com>
2020-04-05 14:23:18 -0400

    Homework03 start

--- lab08/Regression code.py
+++ lab08/Regression code.py
@@ -15,7 +15,7 @@ from keras import models
 from keras import layers
 import numpy as np
 
-(train_data, train_targets), (test_data, test_targets) =  boston_housing.load_data()
+(train_data, train_targets), (test_data, test_targets) = boston_housing.load_data()
 
 # Normalizing the data
 mean = train_data.mean(axis=0)
