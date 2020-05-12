1e1bdd1108e589f53cd015e7d9f6faa3c190035e
tylerpoel <58230944+tylerpoel@users.noreply.github.com>
2020-05-12 14:03:30 -0400

    project - model

---
+++ Homework05/crossword.pl
@@ -0,0 +1,16 @@
+/*
+Homework05 in CS 344 at Calvin University
+Student: Tyler Poel
+Date: May 11, 2020
+*/
+
+%Exercise 2 (LPN! exercise 2.4):
+
+word(astante,  a,s,t,a,n,t,e).
+word(astoria,  a,s,t,o,r,i,a).
+word(baratto,  b,a,r,a,t,t,o).
+word(cobalto,  c,o,b,a,l,t,o).
+word(pistola,  p,i,s,t,o,l,a).
+word(statale,  s,t,a,t,a,l,e).
+
+crossword(V1, V2, V3, H1, H2, H3) :- word(V1)
