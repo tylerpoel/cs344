8a6572e4a7f564076b491d304c245de58559f576
tylerpoel <58230944+tylerpoel@users.noreply.github.com>
2020-05-12 17:27:07 -0400

    Homework-5

--- Homework05/crossword.pl
+++ Homework05/crossword.pl
@@ -13,4 +13,14 @@ word(cobalto,  c,o,b,a,l,t,o).
 word(pistola,  p,i,s,t,o,l,a).
 word(statale,  s,t,a,t,a,l,e).
 
-crossword(V1, V2, V3, H1, H2, H3) :- word(V1)
+crossword(V1, V2, V3, H1, H2, H3) :-
+    word(V1,  _, V1a, _, V1b, _, V1c, _),
+    word(V2,  _, V2a, _, V2b, _, V2c, _),
+    word(V3,  _, V3a, _, V3b, _, V3c, _),
+    word(H1,  _, V1a, _, V2a, _, V3a, _),
+    word(H2,  _, V1b, _, V2b, _, V3b, _),
+    word(H3,  _, V1c, _, V2c, _, V3c, _).
+
+/*
+Entering "crossword(V1, V2, V3, H1, H2, H3)." will give all the possible ways to fill the crossword puzzle.
+*/
